2ec5123747950863d65facb32c3bf2b8adb765de
Sergey Slipchenko <me@faergeek.online>
2021-06-21 08:06:58 +0300

    fix: set stats to errors-only in watch mode

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ function makeConfig({
       : dev
       ? 'browserslist:development'
       : 'browserslist:production',
-    stats: 'errors-warnings',
+    stats: watch ? 'errors-only' : 'errors-warnings',
     devtool: dev ? 'cheap-module-source-map' : 'source-map',
     entry:
       typeof entry === 'string'
